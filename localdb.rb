729168873361febc406d3fd3a44f1770b3cb0f2d
KageShiron <kageshiron@gmail.com>
2016-10-17 14:02:00 +0900

    fix a behavior against doplicate records

--- localdb.rb
+++ localdb.rb
@@ -9,31 +9,31 @@ class RecordDB
     SQLite3::Database.new(dbname) do |db|
       db.execute <<-SQL
         CREATE TABLE record(
-          date INTEGER
-          name TEXT
-          amount INTEGER
+          date INTEGER,
+          name TEXT,
+          amount INTEGER,
           place TEXT
          )
       SQL
     end
   end
 
-  def get_diff( list )
+  def delete_existing( list )
     sql = 'SELECT * from record'
     keys = [:date,:name,:amount,:place]
 
     SQLite3::Database.new(@dbname) do |db|
       db.execute(sql) do |row|
         h = Hash[[keys,row].transpose]
-        list.reject! {|i| i = h }
+        index = list.index(h)
+        list.delete_at(index) if index
       end
     end
-    return list
   end
 
   def add_records( list )
     sql = 'INSERT INTO record values(:date,:name ,:amount ,:place)'
-
+  p list
     SQLite3::Database.new(@dbname) do |db|
       db.transaction do
         list.each do |i|

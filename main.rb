268d3f77bcb70227d54066ff85b488167fe18d88
KageShiron <kageshiron@gmail.com>
2016-10-17 14:51:14 +0900

    fix: String => Symbol fix: log comment

--- main.rb
+++ main.rb
@@ -3,14 +3,29 @@
 
 require "./scraping.rb"
 require "./zaim.rb"
+require "./localdb"
 
+p "open local DB"
+db = RecordDB::new("records.db")
+
+p "start scraping..."
 records = get_record_from_coop
 
+p "delete existing records"
+db.delete_existing( records )
+p records
+
+p "start send to Zaim"
 za = Zaim::new
 records.each do |r|
   p r
-  r["mapping"] = 1
-  r["category_id"] = 101
-  r["genre_id"] = 10104
+  da = r[:date]
+  r[:date] = Time.at(r[:date]).strftime("%Y-%m-%d")
+  r[:mapping] = 1
+  r[:category_id] = 101
+  r[:genre_id] = 10104
   za.create_payment(r)
+  r[:date] = da
 end
+
+db.add_records( records )
